b61470982e6e256968a1574ee737565ae99d4e3c
JuanaGonzalez21 <juanita.juana@hotmail.com>
2023-01-01 21:05:57 -0500

    test port

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/',(req, res)=> {
     res.send('Hola Mundo')
 })
 
-app.listen(8001,()=>{
-    console.log('Server UP running in http://localhost:8001/')
+app.listen(process.env.PORT || 8001,()=>{
+    console.log('Server UP running in http://localhost:8001/ or', process.env.PORT)
 })
